7bb052a70612d01bf2d46e7002fdde200a45d708
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-08 19:58:48 -0800

    added comment box component with tests

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,8 +4,11 @@ import App from '../../src/components/app';
 //use describe to group together similar tests
 describe('App', () => {
   //use it to test a single attribute of a targget
-  it('show the correct test', () => {
+  it('shows the correct test', () => {
+    //create an instance of App
+    const component = renderComponent(App);
+
     //use expect to make an 'assertion' about a target
-    expect
+    expect(component).to.contain('React simple starter');
   });
 });
