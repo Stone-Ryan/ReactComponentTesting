36aa7f0f0ef4521fec11888bbd331b9bdc742b60
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-08 19:02:57 -0800

    set up simple describe block

---
+++ test/components/app_test.js
@@ -0,0 +1,11 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+//use describe to group together similar tests
+describe('App', () => {
+  //use it to test a single attribute of a targget
+  it('show the correct test', () => {
+    //use expect to make an 'assertion' about a target
+    expect
+  });
+});
